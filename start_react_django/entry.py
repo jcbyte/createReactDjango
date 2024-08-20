4058046f006da8aff646fe678f7779329495f277
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-20 20:08:44 +0100

    added argument parsing

--- start_react_django/entry.py
+++ start_react_django/entry.py
@@ -1,2 +1,27 @@
+import argparse
+
+
 def entry():
-    print("Hello world")
+    parser = argparse.ArgumentParser(description="Automate creation of django projects with react")
+
+    parser.add_argument("name", type=str, help="Name of the project")
+    parser.add_argument(
+        "-ts",
+        "--typescript",
+        action="store_true",
+        help="Initialises the frontend with Typescript instead of Javascript",
+    )
+    parser.add_argument(
+        "-cors",
+        action="store_true",
+        help="Enable CORS on the server",
+    )
+    parser.add_argument(
+        "-jwt",
+        action="store_true",
+        help="Includes JWT token setup",
+    )
+
+    args = parser.parse_args()
+
+    print(args.name, args.typescript, args.cors, args.jwt)
