bdc23d26f999d20fe4f86e0fe0dce5cb76ed1d9f
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-20 19:49:23 +0100

    added setup metadata file

---
+++ setup.py
@@ -0,0 +1,17 @@
+# TODO This should be upgraded to a toml
+
+from distutils.core import setup
+
+import setuptools
+
+setup(
+    name="start-react-django",
+    version="0.1.0",
+    description="Automate creation of django projects with react",
+    author="Joel Cutler",
+    packages=["start_react_django"],
+    entry_points={
+        "console_scripts": ["start-react-django=start_react_django.entry:entry"],
+    },
+    install_requires=[],
+)
